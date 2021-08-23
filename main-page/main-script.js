572c07a6af1453f456dca7fa64ea2a27d96e370f
Juarez Goulart <goulartgarcia12@gmail.com>
2021-08-23 01:33:57 -0300

    modal feito

--- main-page/main-script.js
+++ main-page/main-script.js
@@ -9,7 +9,7 @@ function changeBackgroundFlappy() {
     let h2 = document.querySelector('.card-title');
     h1.style.display = "none";
     img.src = hover;
-    img.style.width = "431px";
+    img.style.width = "491px";
     img.style.height = "164px";
     h2.style.color = "#F8F9FF";
    
@@ -59,8 +59,6 @@ function changeBackgroundCommonSnake() {
     h1.innerHTML = "Sala de Jogos";
     h1.style.color = "#8F87F1";
     img.src = start;
-    img.style.width = "139px";
-    img.style.height = "144px";
     h2.style.color = "#8F87F1";
 
     //document.body.style.background = "#F8F9FF";
@@ -71,16 +69,49 @@ var memoryCard = document.querySelector('.memory-game-card');
 memoryCard.addEventListener("mouseover", changeBackgroundMemory);
 
 function changeBackgroundMemory() {
-    document.body.style.background = "url('./assets/memory-game-background.png')";
+    let img = document.querySelector('.img-title');
+    let hover = img.getAttribute('data-hover2');
+    let h2 = document.querySelectorAll('.card-title')[2];
+    let h1 = document.getElementById("main-title");
+    h1.innerHTML = "Jogo da Memória";
+    h1.style.color = "#050AB1";
+    img.src = hover;
+    h2.style.color = "#F8F9FF";
+    //document.body.style.background = "url('./assets/memory-game-background.png')";
 };
 
 memoryCard.addEventListener("mouseleave", changeBackgroundCommonMemory);
 
 function changeBackgroundCommonMemory() {
-    document.body.style.background = "#F8F9FF";
+    let img = document.querySelector('.img-title');
+    let start = img.getAttribute('data-normal');
+    let h2 = document.querySelectorAll('.card-title')[2];
+    let h1 = document.getElementById("main-title");
+    h1.innerHTML = "Sala de Jogos";
+    h1.style.color = "#8F87F1";
+    img.src = start;
+    h2.style.color = "#8F87F1";
+    //document.body.style.background = "#F8F9FF";
 };
 
+//modal
+var modal = document.getElementById("myModal");
 
+function abrirModal() { //função para abrir o modal
+    myModal.style.display = "flex";
+}
+
+//var span = document.getElementsByClassName("close")[0];
+
+function fecharModal() { //função para fechar o modal
+    myModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      myModal.style.display = "none";
+    }
+  }
 
 
 
